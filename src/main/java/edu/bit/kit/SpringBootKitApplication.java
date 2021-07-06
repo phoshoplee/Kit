729a01dafecfb42c8098775dc5e33460ea467ab8
DESKTOP-QF525BP\znwkd <98569856qw!>
2021-07-06 16:49:42 +0900

    Kit 첫번째 Commit

---
+++ src/main/java/edu/bit/kit/SpringBootKitApplication.java
@@ -0,0 +1,13 @@
+package edu.bit.kit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/bit/kit/SpringBootKitApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bit.kit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
